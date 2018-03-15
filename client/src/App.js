49f570805375cccfb7a19daae3c9945ea9ff48d8
Eric Suehiro <ecsuehiro@gmail.com>
2018-03-14 21:22:28 -0700

    Changed some more files
    - React and backend work together now

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import dotenv from 'dotenv'
 import './App.css';
+import { read } from './services/players.service'
 
 dotenv.config()
 
-class App extends Component {
+class App extends PureComponent {
+
+  componentDidMount() { 
+    read()
+  }
+
+
   render() {
     return (
       <div className="App">
--- client/src/services/players.service.js
+++ client/src/services/players.service.js
@@ -1,31 +1,31 @@
-import axios from ('axios')
+import axios from 'axios'
 
 export function read() {
-    return axios.get(process.env.BACKEND_ORIGIN + '/api/players')
+    return axios.get(`${process.env.REACT_APP_BACKEND_ORIGIN}/api/players`)
         .then(onSuccess)
         .catch(onError)
 }
 
 export function readById(id) {
-    return axios.get(process.env.BACKEND_ORIGIN + `/api/players/${id}`)
+    return axios.get(`${process.env.REACT_APP_BACKEND_ORIGIN}/api/players/${id}`)
     .then(onSuccess)
     .catch(onError)
 }
 
 export function create(data) {
-    return axios.post(process.env.BACKEND_ORIGIN + '/api/players', data)
+    return axios.post(process.env.REACT_APP_BACKEND_ORIGIN + '/api/players', data)
     .then(onSuccess)
     .catch(onError)
 }
 
 export function update(id, data) {
-    return axios.put(process.env.BACKEND_ORIGIN + `/api/players/${id}`, data)
+    return axios.put(process.env.REACT_APP_BACKEND_ORIGIN + `/api/players/${id}`, data)
     .then(onSuccess)
     .catch(onError)
 }
 
 export function deactivate(id) {
-    return axios.delete(process.env.BACKEND_ORIGIN + `/api/players/${id}`)
+    return axios.delete(process.env.REACT_APP_BACKEND_ORIGIN + `/api/players/${id}`)
     .then(onSuccess)
     .catch(onError)
 }
